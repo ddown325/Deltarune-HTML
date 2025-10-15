ce71fdbadb838bec77b9f367a5d776de109c1a3c
MichaelD1B <102457@glr.nl>
2025-10-15 21:00:47 +0200

    woops

--- chapter1/savesync.js
+++ chapter1/savesync.js
@@ -11,7 +11,7 @@
     // ============================================================================
     
     const CONFIG = {
-        gameId: 'dtch1',
+        gameId: 'dt',
         debug: true,
         
         // IndexedDB settings
@@ -21,7 +21,7 @@
         },
         
         // localStorage prefix for save files
-        localStoragePrefix: 'dtch1_save_',
+        localStoragePrefix: 'dt_save_',
         
         // Sync intervals
         indexedDBSyncInterval: 10000, // 10 seconds
